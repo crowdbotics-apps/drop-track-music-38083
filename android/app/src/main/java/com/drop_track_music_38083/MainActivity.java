34e6395743164f99fb961a3f01d7c72941e8fc74
Crowdbotics <team@crowdbotics.com>
2022-12-29 19:06:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/drop_track_music_38083/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drop_track_music_38083;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "drop_track_music_38083";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "drop_track_music_38083",
+  url: "https://drop_track_music_38083.botics.co",
+  api: "https://drop_track_music_38083.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
